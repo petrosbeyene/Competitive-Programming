f106485d5a78942c2a7519b645c985665857fb13
unknown <petrosbeyene611@gmail.com>
2022-11-05 23:24:39 +0300

    Add Solution of 'Validate Stack Sequences' problem --leetcode

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validateStackSequences(self, pushed, popped) -> bool:
+        stack = []
+        j = 0
+        for i in range(len(pushed)):
+            stack.append(pushed[i])
+            while stack and j < len(popped) and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+            
+        return j == len(popped)

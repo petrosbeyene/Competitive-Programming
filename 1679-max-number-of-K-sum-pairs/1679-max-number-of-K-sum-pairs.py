4d53709fa6d6a59a8a814a70be2e8ca786321662
petros beyene <91546534+petrosbeyene@users.noreply.github.com>
2023-04-06 17:36:37 +0300

    Create 1679-max-number-of-K-sum-pairs.py

---
+++ 1679-max-number-of-K-sum-pairs/1679-max-number-of-K-sum-pairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        frequency_dict = {}
+        count = 0
+ 
+        for i in nums :
+            if ((i in frequency_dict) and frequency_dict[i] > 0):       
+                frequency_dict[i] -= 1
+                count += 1
+            elif k - i in frequency_dict:
+                frequency_dict[k - i] += 1
+            else:
+                frequency_dict[k - i] = 1
+     
+        return count
+                    
+        

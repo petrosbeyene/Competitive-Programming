1c4b953d7652f2b80363a63835cc6ac7630c19c4
petros beyene <91546534+petrosbeyene@users.noreply.github.com>
2023-04-17 22:20:20 +0300

    Create 2428-maximum-sum-of-an-hourglass.py

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        temp = 0
+        ans = float("-inf")
+        for i in range(len(grid)-2):
+            for j in range(1, len(grid[0])-1):
+                temp = sum(grid[i][j-1:j+2]) + grid[i+1][j] + sum(grid[i+2][j-1:j+2])
+                ans = max(ans, temp)
+        
+        return ans

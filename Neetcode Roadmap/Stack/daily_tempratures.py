61c06b0fb2cd4089d4d79bdf663e71779e1fa31f
petros beyene <petrosbeyene611@gmail.com>
2025-03-20 14:17:31 +0300

    add neetcode roadmap stack solutions

---
+++ Neetcode Roadmap/Stack/daily_tempratures.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        monotonic_stack = []
+        answer = [0] * len(temperatures)
+        for i, val in enumerate(temperatures):
+            while monotonic_stack and val > temperatures[monotonic_stack[-1]]:
+                curr = monotonic_stack.pop()
+                answer[curr] = i - curr
+
+            monotonic_stack.append(i)
+        
+        return answer
+
+        #Time complexity = O(N) -> worst case scenario = Amortized time complexity
+        #Space complexity = O(N)
+
+                
---
+++ Neetcode Roadmap/Stack/evaluate_reverse_polish_notation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operators = {'+', '-', '*', '/'}
+
+        for token in tokens:
+            if token in operators:
+                val2, val1 = stack.pop(), stack.pop()
+                if token == '+':
+                    stack.append(val1 + val2)
+                elif token == '-':
+                    stack.append(val1 - val2)
+                elif token == '*':
+                    stack.append(val1 * val2)
+                else:
+                    stack.append(int(val1 / val2))
+            
+            else:
+                stack.append(int(token))
+
+        return stack[0]

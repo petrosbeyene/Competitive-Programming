b7220a7f4e1d7397f5ab63863220dee73f1421a1
petros beyene <petrosbeyene611@gmail.com>
2025-04-10 21:29:11 +0300

    Add neetcode/array&hashmap/two_sum and group_anagram problem solutions

---
+++ Neetcode Roadmap/Array&Hasmap/group_anagram.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        anagram_map = defaultdict(list)
+        for word in strs:
+            curr = "".join(sorted(word))
+            anagram_map[curr].append(word)
+        
+        return list(anagram_map.values())
+
+        # Time complexity: O(n*mlogm), when n is the length of strs array, and m is the length of the longest string
+        # Space complexity: O(n*m), same assumption as the above
---
+++ Neetcode Roadmap/Array&Hasmap/two_sum.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+
+        for i, num in enumerate(nums):
+            if target - num in num_map:
+                return [num_map[target - num], i]
+            
+            num_map[num] = i
+        
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
